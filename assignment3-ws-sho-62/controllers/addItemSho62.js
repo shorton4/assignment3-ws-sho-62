c5e610ace0b5389268018604a4c68a103d770151
Steve Horton <shorton4@student.douglascollege.ca>
2021-03-22 13:14:49 -0700

    Added addItemSho62 with POST request

---
+++ assignment3-ws-sho-62/controllers/addItemSho62.js
@@ -0,0 +1,9 @@
+
+//Add to cart
+//adds an item to the cart
+app.post('/cartSho62', (req, res) =>{
+    var newStockItem = {sku: req.body.sku, name: req.body.name, price: req.body.price, quantity: req.body.quantity};
+    stock.push(newStockItem);
+    res.header("Content-Type","application/json");
+    res.send(stock);
+})
