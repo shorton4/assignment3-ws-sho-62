d7d3fd0b6ef1e1e21ac7cbaa8757b674d8e09ab1
Steve Horton <shorton4@student.douglascollege.ca>
2021-03-22 13:15:41 -0700

    Added getCartSho62 with GET request

---
+++ assignment3-ws-sho-62/controllers/getCartSho62.js
@@ -0,0 +1,9 @@
+
+//get cart
+//returns the shopping cart with relevant data
+app.get('/cartSho62', (req,res) =>{
+    console.log("GET request for: ");
+    console.log(JSON.stringify(stock,null, '\t'));
+    res.header("Content-Type","application/json");
+    res.send(stock);
+})
